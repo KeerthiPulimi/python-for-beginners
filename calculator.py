66c43293ebdae59944969978ac26f07818780625
KeerthiPulimi <pulimikeerthi155@gmail.com>
2024-04-15 15:30:42 +0530

    square of number

---
+++ calculator.py
@@ -0,0 +1,24 @@
+#add numbers 
+x= int(input("whats the value of x?"))
+y= int(input("whats the value of y?"))
+print(x+y)
+
+#doing float additions
+x1= float(input("whats the value of x?"))
+y= float(input("whats the value of y?"))
+z = round(x+y)
+print(z)
+print(f"{z:,}")
+
+z=x/y
+print(f"{z:.2f}")
+
+# square of number 
+def main():
+  x=int(input("what is x?"))
+  print("x squared is", square(x))
+
+def square(n):
+  a=n*n
+  return a 
+main(  )
--- hello.py
+++ hello.py
@@ -1,22 +1,28 @@
 #Ask user for their name
-name= input("what is ypur name?")
+#name= input("what is ypur name?")...
 
 #say hello to user 
-print("hello,", name)
+#print("hello,", name)
 
 #some other ways - here this strip removes any spaces that user has given while writting the name from front and back
-name = name.strip()
-print(f"hello, {name}")
+#name = name.strip()
+#print(f"hello, {name}")
  
  #still the names first letter is small but to make it capital we use this
-name = name.capitalize()
-print(f"hello, {name}")
+#name = name.capitalize()
+#print(f"hello, {name}")
 
 # the capitalize works on;y when u give one name but if you give 2 naes and want to amek both the first letters of the name capital its better that we use title
 
-name = name.title()
-print(f"hello, {name}")   
+#name = name.title()
+#print(f"hello, {name}")  
 
-x= int(input("whats tghe value of x?"))
-y= int(input("whats the value of y?"))
-print(x+y)
+
+def hello(to):
+   print("hello,",to)
+
+name1=input("what is your name?")
+hello(name1)
+
+  
+  
