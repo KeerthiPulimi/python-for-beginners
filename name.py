40848ffd80b67b43d62a33d91198f545ca4c02ae
KeerthiPulimi <pulimikeerthi155@gmail.com>
2024-04-19 14:49:25 +0530

    added for loop

--- name.py
+++ name.py
@@ -8,7 +8,10 @@ import sys
 
 if len(sys.argv)<2:
   sys.exit("Too few arguments")
-elif len(sys.argv)>2:
-  sys.exit("Too many arguments") 
-else:
-  print("hello, my anme is", sys.argv[1])     
+# elif len(sys.argv)>2:
+#   sys.exit("Too many arguments") 
+# else:
+#   print("hello, my anme is", sys.argv[1])     
+
+for arg in sys.argv[1:]:
+  print("hello , my name is", arg)
