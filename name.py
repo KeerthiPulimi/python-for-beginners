2ad40947438227633d0969c575fdd4c7a58d9544
KeerthiPulimi <pulimikeerthi155@gmail.com>
2024-04-19 10:12:21 +0530

    with if,elif,else block

--- name.py
+++ name.py
@@ -1,7 +1,14 @@
 import sys
 
-try:
-  print("hello, my name is",sys.argv[1])
+# try:
+#   print("hello, my name is",sys.argv[1])
 
-except IndexError:
-  print("too few arguments") 
+# except IndexError:
+#   print("too few arguments") 
+
+if len(sys.argv)<2:
+  print("Too few arguments")
+elif len(sys.argv)>2:
+  print("Too many arguments") 
+else:
+  print("hello, my anme is", sys.argv[1])   
