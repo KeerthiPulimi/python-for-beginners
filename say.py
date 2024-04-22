0459011284fda2081deebf53c026bd62c2caf54a
KeerthiPulimi <pulimikeerthi155@gmail.com>
2024-04-22 14:02:46 +0530

    created a new file

--- say.py
+++ say.py
@@ -1,15 +1,16 @@
 import cowsay
 import sys
-from saying import hello
+# from saying import hello
+from saying import goodbye
 
 # if len(sys.argv) == 2:
 #     message = "hello, " + sys.argv[1]
 #     cowsay.tux(message)
-#     cowsay.cow(message)
+#     cowsacow(message)
 #     cowsay.dragon(message)
 # else:
 #     print("Usage: python say.py [name]")
 
 
 if len(sys.argv) == 2:
-  hello(sys.argv[1])
+  goodbye(sys.argv[1])
