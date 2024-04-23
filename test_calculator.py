ef50a6b71aee6eb9ae083275a862a03a0f4726bf
KeerthiPulimi <pulimikeerthi155@gmail.com>
2024-04-23 14:53:09 +0530

    imported

--- test_calculator.py
+++ test_calculator.py
@@ -1,22 +1,27 @@
 from calculator import square
-
-
-def main():
-  test_square()
-
-
 def test_square():
-  try:
-    assert square(2) == 4
-  except AssertionError:
-    print("2 auared wa not 4")
+  assert square(2) ==4
+  assert square(3) ==9
+  assert square(-2) ==4
   
-  
-  try:
-    assert square(3) == 9
-  except AssertionError:
+  assert square(-3) ==9
+  assert square(0) ==0
+# def main():
+#   test_square()
 
-    print("3 squared was notb9")  
+
+# def test_square():
+#   try:
+#     assert square(2) == 4
+#   except AssertionError:
+#     print("2 auared wa not 4")
+  
+  
+#   try:
+#     assert square(3) == 9
+#   except AssertionError:
+
+#     print("3 squared was notb9")  
 
 
 # def test_square():
@@ -25,7 +30,7 @@ def test_square():
 #   if square(3) != 9:
 #     print("3 squared is not 9")
 
-if __name__ == "__main__":
-  main()
+# if __name__ == "__main__":
+#   main()
 
     
