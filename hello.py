c7bfa8ecd16a0e692a0702971b05f9496f2f292a
KeerthiPulimi <pulimikeerthi155@gmail.com>
2024-05-09 13:01:06 +0530

    testpython

--- hello.py
+++ hello.py
@@ -25,15 +25,15 @@
 # hello(name1)
 
 
-
 def main():
-  name=input("what is your name?")
-  hello(name)
+    name = input("What is your name?")
+    print(hello(name))
+
 def hello(to="world"):
-  print("hello,",to)
+    return f"hello, {to}"
 
 if __name__ == "__main__":
-  main()
+    main()
 
  
   
